c25872701c65c66303e407aa39a8c666c97865c2
David Tanioka <dtanioka7@gmail.com>
2026-08-30 18:42:48 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int buy = 0;
+        for(int sell = 1; sell<prices.length; sell++){
+            if(prices[sell] < prices[buy]){
+                buy = sell;
+            }
+            max = Math.max(max, prices[sell]-prices[buy]);
+        }
+        return max;
+    }
+}
